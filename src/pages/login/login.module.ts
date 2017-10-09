e8176d656d740e33b3162b732b34eebd797559cb
Steve Redstorm <steve.redstorm@gmail.com>
2017-10-10 00:02:33 +0200

    Adding login icon and fixing errors

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-
+import { LoginPage } from '../../routes';
 @NgModule({
   declarations: [
     LoginPage,
